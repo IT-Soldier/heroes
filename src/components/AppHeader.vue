35fb1d5fdad82c03f6f94e7d71e74ea992ddb59a
hanchao <1650070770@qq.com>
2018-09-02 20:51:56 +0800

    非路由组件的处理

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <nav class="navbar navbar-inverse navbar-fixed-top">
+        <div class="container-fluid">
+            <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">CRUD</a>
+            </div>
+        </div>
+        </nav>
+    </div>
+</template>
+
+<script>
+//头部导航栏组件
+//  这是一个组件模块,需要将接口暴露出去,即是东西少最好也应该这样做
+export default {
+    
+}
+</script>
+
+
+
+<style>
+
+</style>
