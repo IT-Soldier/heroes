59c36f7576b210b94884cae774089103b0e578bd
hanchao <1650070770@qq.com>
2018-09-03 01:52:32 +0800

    添加功能完成

--- src/views/heroes/Add.vue
+++ src/views/heroes/Add.vue
@@ -1,19 +1,50 @@
 <template>
     <form>
         <div class="form-group">
-            <label for="exampleInputEmail1">英雄名称</label>
-            <input type="text" class="form-control" placeholder="英雄名称">
+            <label for="heroName">英雄名称</label>
+            <input type="text" class="form-control" id="heroName" placeholder="英雄名称" v-model="formData.name">
         </div>
         <div class="form-group">
-            <label for="exampleInputPassword1">英雄性别</label>
-            <input type="text" class="form-control" placeholder="英雄性别">
+            <label for="heroGender">英雄性别</label>
+            <input type="text" class="form-control" id="heroGender" placeholder="英雄性别" v-model="formData.gender">
         </div>
-        <button type="submit" @click="addHandel" class="btn btn-success">Submit</button>
+        <!-- 去阻止submit的默认提交事件 -->
+        <button type="submit" @click.prevent="addHandel" class="btn btn-success">Submit</button>
     </form>
 </template>
 
 <script>
+
+import axios from 'axios';
+import router from 'vue-router';
+
 export default {
+    data() {
+        return {
+            // 定义一个对象,去拿表单的值,方便post传递值
+            formData : {
+                name : '',
+                gender : ''
+            }
+        }
+    },
+    methods : {
+        addHandel() {
+            axios
+                .post('http://localhost:3001/heroes',this.formData)
+                .then(response => {
+                    // post添加数据成功,状态码为201
+                    if(response.status === 201) {
+                        // 使用编程式导航,跳回列表页面(用该方法失败了)router.push('/heroes'),why
+                        // console.log(this.$router)
+                        this.$router.push('/heroes')
+                    }
+                })
+                .catch(error => {
+                    console.log(error)
+                })
+        }
+    }
 
 }
 </script>
--- src/views/heroes/List.vue
+++ src/views/heroes/List.vue
@@ -1,7 +1,9 @@
 <template>
     <div>
         <h2 class="sub-header">英雄列表</h2>
-        <a class="btn btn-success" href="add.html">Add</a>
+        <!-- <a class="btn btn-success" href="add.html">Add</a> -->
+        <!-- router-link标签,to属性必须要有,否则标签的状态不会生效 -->
+        <router-link class="btn btn-success" to="/heroes/add">Add</router-link>
         <div class="table-responsive">
             <table class="table table-striped">
                 <thead>
